04e6320682f3b2a69a433803f77d14b43774efe1
Kotzent Manuel <mankotzent@edu.aau.at>
2025-05-07 12:33:47 +0200

    Slight change in setup

--- src/main/java/at/aau/serg/websocketserver/websocket/broker/actioncard/ActionCardControler.java
+++ src/main/java/at/aau/serg/websocketserver/websocket/broker/actioncard/ActionCardControler.java
@@ -1,4 +1,4 @@
 package at.aau.serg.websocketserver.websocket.broker.actioncard;
 
-public enum ActionCardActions {
+public class ActionCardControler {
 }
--- src/main/java/at/aau/serg/websocketserver/websocket/broker/actioncard/ActionCardDeck.java
+++ src/main/java/at/aau/serg/websocketserver/websocket/broker/actioncard/ActionCardDeck.java
@@ -1,4 +1,4 @@
 package at.aau.serg.websocketserver.websocket.broker.actioncard;
 
-public class ActionCardManager {
+public class ActionCardDeck {
 }
