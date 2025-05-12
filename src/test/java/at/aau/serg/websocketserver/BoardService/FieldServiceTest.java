e36ea3004f89c98a9cb6be3bc827983d17c8fafb
Chris <Chris@DESKTOP-8D2BEIC>
2025-05-12 19:27:46 +0200

    refactor BoardServiceTest zu FieldServiceTest

--- src/test/java/at/aau/serg/websocketserver/BoardService/FieldServiceTest.java
+++ src/test/java/at/aau/serg/websocketserver/BoardService/FieldServiceTest.java
@@ -9,7 +9,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class BoardServiceTest {
+public class FieldServiceTest {
 
     private PlayerService playerService;
     private FieldService fieldService;
