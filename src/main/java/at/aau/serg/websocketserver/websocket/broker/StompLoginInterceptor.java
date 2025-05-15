c65829eee9ae288b3f7d8ad53b53db58ea5ec4a9
the-only-queen-anna <annpschernig@edu.aau.at>
2025-05-15 14:43:01 +0200

    sonar quality check issues resolved

--- src/main/java/at/aau/serg/websocketserver/websocket/broker/StompLoginInterceptor.java
+++ src/main/java/at/aau/serg/websocketserver/websocket/broker/StompLoginInterceptor.java
@@ -2,6 +2,7 @@ package at.aau.serg.websocketserver.websocket.broker;
 
 import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.simp.stomp.StompCommand;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.messaging.support.ChannelInterceptor;
 import org.springframework.messaging.support.MessageHeaderAccessor;
@@ -18,20 +19,24 @@ public class StompLoginInterceptor implements ChannelInterceptor {
         StompHeaderAccessor accessor =
                 MessageHeaderAccessor.getAccessor(message, StompHeaderAccessor.class);
 
-        if (accessor != null && accessor.getCommand() != null) {
-            switch (accessor.getCommand()) {
-                case CONNECT:
-                    String login = accessor.getFirstNativeHeader("login");
-                    if (login == null || login.isBlank()) {
-                        login = "Anonymous-" + System.currentTimeMillis();
-                    }
+        if (accessor != null) {
+            StompCommand command = accessor.getCommand();
+            accessor.getCommand();
+            if(command != null) {
+                switch (command) {
+                    case CONNECT:
+                        String login = accessor.getFirstNativeHeader("login");
+                        if (login == null || login.isBlank()) {
+                            login = "Anonymous-" + System.currentTimeMillis();
+                        }
 
-                    // Setzt das Principal für diese STOMP-Session
-                    String finalLogin = login;
-                    accessor.setUser(() -> finalLogin);
-                    break;
-                default:
-                    break;
+                        // Setzt das Principal für diese STOMP-Session
+                        String finalLogin = login;
+                        accessor.setUser(() -> finalLogin);
+                        break;
+                    default:
+                        break;
+                }
             }
         }
 
