e7bd28db1c91c4edc673176a3d47599b62f19543
ManKotzent <137501602+ManKotzent@users.noreply.github.com>
2025-06-05 12:34:49 +0200

    Expanded controller logic to support multiple games; Created layouts for project member dependencies Created method layout for playing card

--- src/main/java/at/aau/serg/websocketserver/websocket/broker/ActionCardController.java
+++ src/main/java/at/aau/serg/websocketserver/websocket/broker/ActionCardController.java
@@ -8,35 +8,60 @@ import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 
+import java.util.IdentityHashMap;
+
 /**
  * Controller to handle drawing of action cards over WebSocket.
  */
 @Controller
 public class ActionCardController {
 
-    private final ActionCardDeck deck;
+    private final IdentityHashMap<String, ActionCardDeck> decks;
+    private final IdentityHashMap<String, ActionCard> pulledCards;
     private final SimpMessagingTemplate messagingTemplate;
 
     @Autowired
     public ActionCardController(ActionCardDeck deck,
                                 SimpMessagingTemplate messagingTemplate) {
-        this.deck = deck;
+        this.decks = new IdentityHashMap<>();
+        this.pulledCards = new IdentityHashMap<>();
         this.messagingTemplate = messagingTemplate;
     }
 
     /**
-     * Handles draw card requests. Clients send to /app/drawCard/{playerName}.
-     * The drawn card is sent back to /topic/card/{playerName}.
+     * Handles draw card requests. Clients send to /app/drawCard/{gameSession}/{playerName}.
+     * The drawn card is sent back to /topic/card/{gameSession}/{playerName}.
      *
+     * @param gameSession the identifier for the game where card is drawn
      * @param playerName the identifier of the player drawing the card
      */
-    @MessageMapping("/drawCard/{playerName}")
-    public void drawCard(@DestinationVariable String playerName) {
+    @MessageMapping("/drawCard/{gameSession}/{playerName}")
+    public void drawCard(@DestinationVariable String gameSession, @DestinationVariable String playerName) {
+        //TODO: Does session exist? If so, does a deck for this game exist? If no, add one.
+
+        //TODO: Does player exist in this game?
+
+        //TODO: Is it player's turn?
+
+        ActionCardDeck deck = decks.get(gameSession);
+
         // Pull next card from the deck
         ActionCard card = deck.pull();
 
         // Send the card back to the specific player topic
-        String destination = String.format("/topic/card/%s", playerName);
+        String destination = String.format("/topic/card/%s/%s", gameSession, playerName);
         messagingTemplate.convertAndSend(destination, card);
+    }
+
+    @MessageMapping("/playCard/{gameSession}/{playerName}")
+    public void playCard(@DestinationVariable String gameSession, @DestinationVariable String playerName) {
+        //TODO: Does session exist?
+
+        //TODO: Does player exist in this game?
+
+        //TODO: Is it player's turn?
+
+        //TODO: Is a card currently pulled in this game?
+        
     }
 }
