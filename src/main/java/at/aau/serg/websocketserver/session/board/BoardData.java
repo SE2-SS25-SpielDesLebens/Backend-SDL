1962493a8552910f372e50e65b5079731f48d594
the-only-queen-anna <annpschernig@edu.aau.at>
2025-06-08 20:37:19 +0200

    more issues resolved

--- src/main/java/at/aau/serg/websocketserver/session/board/BoardData.java
+++ src/main/java/at/aau/serg/websocketserver/session/board/BoardData.java
@@ -205,12 +205,12 @@ public class BoardData implements BoardDataProvider {    private static final Li
     /**
      * Statische Methode für Abwärtskompatibilität, sollte in neuen Code nicht verwendet werden.
      * Verwenden Sie stattdessen die Instanzmethode.
-     *
-     * @param index Der Index des gesuchten Felds
+     *     * @param index Der Index des gesuchten Felds
      * @return Das gefundene Feld oder null, wenn kein Feld mit diesem Index existiert
      * @deprecated Verwende die Instanz über Dependency Injection
+     * @since 1.0
      */
-    @Deprecated
+    @Deprecated(since = "1.0", forRemoval = false)
     public static Field getFieldByIndexStatic(int index) {
         for (Field field : BOARD) {
             if (field.getIndex() == index) {
