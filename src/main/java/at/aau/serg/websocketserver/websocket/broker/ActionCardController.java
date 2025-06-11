7d3bfb9efb1d56c9b83a00a5d854e4c99c4b2901
ManKotzent <137501602+ManKotzent@users.noreply.github.com>
2025-06-11 15:12:57 +0200

    Implemented PlayerService into Action Card logic

--- src/main/java/at/aau/serg/websocketserver/websocket/broker/ActionCardController.java
+++ src/main/java/at/aau/serg/websocketserver/websocket/broker/ActionCardController.java
@@ -1,5 +1,6 @@
 package at.aau.serg.websocketserver.websocket.broker;
 
+import at.aau.serg.websocketserver.player.PlayerService;
 import at.aau.serg.websocketserver.session.actioncard.ActionCard;
 import at.aau.serg.websocketserver.session.actioncard.ActionCardDeck;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,12 +21,15 @@ public class ActionCardController {
     private final IdentityHashMap<String, ActionCard> pulledCards;
     private final SimpMessagingTemplate messagingTemplate;
 
+    private final PlayerService playerService;
+
     @Autowired
     public ActionCardController(ActionCardDeck deck,
                                 SimpMessagingTemplate messagingTemplate) {
         this.decks = new IdentityHashMap<>();
         this.pulledCards = new IdentityHashMap<>();
         this.messagingTemplate = messagingTemplate;
+        this.playerService = PlayerService.getInstance();
     }
 
     /**
@@ -33,10 +37,10 @@ public class ActionCardController {
      * The drawn card is sent back to /topic/card/{gameSession}/{playerName}.
      *
      * @param gameSession the identifier for the game where card is drawn
-     * @param playerName the identifier of the player drawing the card
+     * @param playerId the identifier of the player drawing the card
      */
-    @MessageMapping("/drawCard/{gameSession}/{playerName}")
-    public void drawCard(@DestinationVariable String gameSession, @DestinationVariable String playerName) {
+    @MessageMapping("/drawCard/{gameSession}/{playerId}")
+    public void drawCard(@DestinationVariable String gameSession, @DestinationVariable String playerId) {
         //TODO: Does session exist? If so, does a deck for this game exist? If no, add one.
 
         //TODO: Does player exist in this game?
@@ -49,13 +53,15 @@ public class ActionCardController {
         ActionCard card = deck.pull();
 
         // Send the card back to the specific player topic
-        String destination = String.format("/topic/card/%s/%s", gameSession, playerName);
+        String destination = String.format("/topic/card/%s/%s", gameSession, playerId);
         messagingTemplate.convertAndSend(destination, card);
     }
 
-    @MessageMapping("/playCard/{gameSession}/{playerName}")
-    public void playCard(@DestinationVariable String gameSession, @DestinationVariable String playerName) {
+    @MessageMapping("/playCard/{gameSession}/{playerId}/{decision}")
+    public void playCard(@DestinationVariable String gameSession, @DestinationVariable String playerId, @DestinationVariable String decision) {
         //TODO: Does session exist?
+
+        playerService.isPlayerRegistered(playerId);
 
         //TODO: Does player exist in this game?
 
